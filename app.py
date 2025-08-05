c77a9e150124a825e9dd959a86f97b0e20a1d954
ZeiParaPadlet <ZeiPara.padlet@outlook.jp>
2025-08-05 10:41:18 +0900

    Update app.py

--- app.py
+++ app.py
@@ -10,6 +10,12 @@ import os
 
 app = FastAPI()
 
+scale_num = [10, 20, 30, 40, 45, 50, 55, 60, 70]
+scale_name = ["震度1", "震度2", "震度3", "震度4", "震度5弱", "震度5強", "震度6弱", "震度6強", "震度7"]
+
+def scale_num2name(input):
+    return scale_name[scale_num.index(input)]
+
 @app.get("/")
 async def read_root():
     return {"Hello": "World"}
@@ -28,27 +34,19 @@ def on_message(ws, message):
             time = re.split('/ :', message.earthquake.time)
             dt_object = datetime.strptime(time, "%Y/%m/%d %H:%M:%S")
             time_str = dt_object.strftime("%Y年%m月%d日%H時%M分%S秒")
-            
+            points = message.points
+            point_str = ""
+            for point in points:
+                point_str = point_str + point.pref + point.addr + ":" + scale_num2name(point.scale) + "\n"
 
 def on_error(ws, error):
-    """
-    エラーが発生したときに実行される関数。
-    """
     print(f"エラー: {error}")
 
 def on_close(ws, close_status_code, close_msg):
-    """
-    接続が閉じられたときに実行される関数。
-    """
     print("接続が閉じられました")
 
 def on_open(ws):
-    """
-    接続が確立されたときに実行される関数。
-    """
     print("接続が確立されました")
-    # 接続後、すぐにメッセージを送信することも可能
-    ws.send("hello")
 
 if __name__ == "__main__":
     uri = "wss://api.p2pquake.net/vs/ws" # テスト用の公開サーバー
